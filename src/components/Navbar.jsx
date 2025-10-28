6868d62b1be20ab931696ebf41231314d8928ca9
Senthil <senthilkumari24p@gmail.com>
2025-10-28 17:24:12 -0400

    feat: add header component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+export function Navbar() {
+  return (
+    <header>
+      <nav>
+        <img src="/src/assets/react.svg" alt="react logo" />
+        <span>ReactFacts</span>
+      </nav>
+    </header>
+  );
+}
