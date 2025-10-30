4927b68fbe4aac914fcd12d2edbbbdf347c71756
Senthil <senthilkumari24p@gmail.com>
2025-10-30 17:26:30 -0400

    feat: add footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Footer } from "./components/Footer";
 import { Main } from "./components/Main";
 import { Navbar } from "./components/Navbar";
 
@@ -6,6 +7,7 @@ function App() {
     <>
       <Navbar />
       <Main />
+      <Footer />
     </>
   );
 }
